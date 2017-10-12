40671418a7505eb34dd4239e296d895dfd79504b
Wootsz <wouterbrozius@hotmail.com>
2017-10-12 19:11:48 +0200

    B -> J

--- Klondike_Solitaire_Simulation/Klondike_Solitaire_Simulation/Card.cs
+++ Klondike_Solitaire_Simulation/Klondike_Solitaire_Simulation/Card.cs
@@ -9,7 +9,7 @@ using static Onderzoeksmethoden.Rank;
 namespace Onderzoeksmethoden
 {
     public enum Suit { Spades, Hearts, Clubs, Diamonds }
-    public enum Rank { A, _2, _3, _4, _5, _6, _7, _8, _9, _10, B, Q, K }
+    public enum Rank { A, _2, _3, _4, _5, _6, _7, _8, _9, _10, J, Q, K }
 
     class Card
     {
