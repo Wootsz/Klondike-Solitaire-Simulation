e240fb95a9575363fc995cad8d4e09ba635daa79
Qub1 <qub1995@gmail.com>
2017-10-15 15:20:43 +0200

    Added generate deck method. Started working on RNG.

--- Klondike_Solitaire_Simulation/RNG.cs
+++ Klondike_Solitaire_Simulation/RNG.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Klondike_Solitaire_Simulation
 {
-	public class RNG {
+	public static class RNG {
 		private const int a = 2416;
 		private const int c = 374441;
 		private const int m = 1771875;
@@ -14,10 +14,15 @@ namespace Klondike_Solitaire_Simulation
 		/// <summary>
 		/// The current seed.
 		/// </summary>
-		private int current_seed;
+		private static int current_seed;
 
-		public RNG(int seed = -1)
+		/// <summary>
+		/// Creates a new random generator.
+		/// </summary>
+		/// <param name="seed">The given seed.</param>
+		static RNG(int seed = -1)
 		{
+			// Check if a seed has been given
 			if (seed < 0)
 			{
 				TimeSpan span = DateTime.Now.Subtract(new DateTime(1970, 1, 1, 0, 0, 0));
@@ -32,33 +37,19 @@ namespace Klondike_Solitaire_Simulation
 		/// <summary>
 		/// Get the next 'random' number in line.
 		/// </summary>
-		public int GetNumber()
-		{
-			// Recalculate the seed
-			current_seed = (a * current_seed + c) % m;
-
-			// Return the seed as number
-			return current_seed;
-		}
+		public int GetNumber() => current_seed = (a * current_seed + c) % m;
 
 		/// <summary>
 		/// Returns a 'random' number between 0 and 1.
 		/// </summary>
-		public float Next()
-		{
-			// Generate float number
-			return GetNumber() / 1000000.0f;
-		}
+		public float Next() => GetNumber() / 1000000.0f;
 
 		/// <summary>
 		/// Returns a 'random' number between 0 (inclusive) and max.
 		/// </summary>
 		/// <param name="max">The upper boundary (exclusive).</param>
 		/// <returns>A random number.</returns>
-		public int Next(int max)
-		{
-			return Next(0, max);
-		}
+		public int Next(int max) => Next(0, max);
 
 		/// <summary>
 		/// Returns a 'random' number between min and max.
@@ -66,9 +57,6 @@ namespace Klondike_Solitaire_Simulation
 		/// <param name="min">The lower boundary (inclusive).</param>
 		/// <param name="max">The upper boundary (exclusive).</param>
 		/// <returns>A random number.</returns>
-		public int Next(int min, int max)
-		{
-			return min + GetNumber() % max;
-		}
+		public int Next(int min, int max) => min + GetNumber() % max;
 	}
 }
--- Klondike_Solitaire_Simulation/State.cs
+++ Klondike_Solitaire_Simulation/State.cs
@@ -10,7 +10,7 @@ namespace Klondike_Solitaire_Simulation
 {
 	public class State
 	{
-		public List<Card> deck = new List<Card>();
+		public List<Card> deck = GenerateStandardDeck();
 		int no_of_tableaus = 7;
 
 		public List<Card> stock = new List<Card>();
@@ -20,18 +20,31 @@ namespace Klondike_Solitaire_Simulation
 		public Stack<Card> foundation_hearts = new Stack<Card>();
 		public List<Stack<Card>> tableaus = new List<Stack<Card>>();
 
-		public State()
+		/// <summary>
+		/// Generates a default deck with all 52 cards.
+		/// </summary>
+		/// <returns>A standard deck.</returns>
+		public static List<Card> GenerateStandardDeck()
 		{
+			List<Card> result = new List<Card>();
+
 			// Make a deck with every card
-			deck = new List<Card>();
 			for (int i = 0; i < Enum.GetNames(typeof(Suit)).Length; i++)
 			{
 				for (int j = 0; j < Enum.GetNames(typeof(Rank)).Length; j++)
 				{
-					deck.Add(new Card((Suit)i, (Rank)j));
+					result.Add(new Card((Suit)i, (Rank)j));
 				}
 			}
 
+			return result;
+		}
+
+		/// <summary>
+		/// Creates a new game state.
+		/// </summary>
+		public State()
+		{
 			ShuffleDeck();
 
 			// Fill tableaus
@@ -51,9 +64,25 @@ namespace Klondike_Solitaire_Simulation
 			stock = deck;
 		}
 
+		/// <summary>
+		/// Shuffles a deck.
+		/// </summary>
 		public void ShuffleDeck()
 		{
+			
+		}
 
+		/// <summary>
+		/// Gets a string representation of the current state.
+		/// </summary>
+		/// <returns>The current state as a string.</returns>
+		public override string ToString() {
+			string output = "";
+
+			// Output deck
+			output += "Deck: " + String.Join(", ", deck);
+
+			return output;
 		}
 	}
 }
