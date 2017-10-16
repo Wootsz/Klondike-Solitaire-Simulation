3b28c9c9ac79e617a64985ee8e39a4f111ff309a
Wootsz <wouterbrozius@hotmail.com>
2017-10-16 16:31:41 +0200

    Program is back

---
+++ Klondike_Solitaire_Simulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Klondike_Solitaire_Simulation.Suit;
+using static Klondike_Solitaire_Simulation.Rank;
+
+namespace Klondike_Solitaire_Simulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Klondike Solitaire Simulator!");
+
+            int iterations = 1;
+
+            for (int iteration = 0; iteration < iterations; iteration++)
+            {
+                // Initialize the game state
+                State state = new State();
+
+                Console.WriteLine(state.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
