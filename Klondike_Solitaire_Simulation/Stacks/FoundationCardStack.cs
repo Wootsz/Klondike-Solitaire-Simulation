72e799bd0f9620bf7b4cd996b0497b482291aa2d
Qub1 <qub1995@gmail.com>
2017-10-16 17:17:01 +0200

    Made stack-specific classes.

---
+++ Klondike_Solitaire_Simulation/Stacks/FoundationCardStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klondike_Solitaire_Simulation.Stacks
+{
+	public class FoundationCardStack : CardStack
+	{
+		/// <summary>
+		/// Checks if a move is possible given a card.
+		/// </summary>
+		/// <param name="card">The Card you want to move to the stack.</param>
+		/// <returns></returns>
+		public bool IsMovePossible(Card card)
+		{
+			if (IsEmpty())
+			{
+				return card.rank == Rank.Ace;
+			}
+
+			return card.suit == Peek().TopCard().suit && card.rank == Peek().TopCard().rank + 1;
+		}
+	}
+}
