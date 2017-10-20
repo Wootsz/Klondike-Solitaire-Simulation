d7a6e5d6cace7a991170f5fd67b25afa596dcf8a
Wootsz <wouterbrozius@hotmail.com>
2017-10-20 17:07:59 +0200

    WindowsScoreheuristiek

--- Klondike_Solitaire_Simulation/Program.cs
+++ Klondike_Solitaire_Simulation/Program.cs
@@ -6,46 +6,99 @@ using System.Text;
 using System.Threading.Tasks;
 using static Klondike_Solitaire_Simulation.Suit;
 using static Klondike_Solitaire_Simulation.Rank;
+using Klondike_Solitaire_Simulation.Stacks;
+
 
 namespace Klondike_Solitaire_Simulation
 {
-	public class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.OutputEncoding = System.Text.Encoding.UTF8;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-			Console.WriteLine("Welcome to the Klondike Solitaire Simulator!");
-			Console.Title = "Klondike Solitaire Simulator";
+            Console.WriteLine("Welcome to the Klondike Solitaire Simulator!");
+            Console.Title = "Klondike Solitaire Simulator";
 
-			Random r = new Random();
+            RNG r = new RNG();
 
-			// Initialize the game state
-			State startState = new State();
+            // Initialize the game state
+            //State startState = new State();
 
-			Console.WriteLine("Start state:");
-			Console.WriteLine(startState.ToString(false));
+            //Console.WriteLine("Start state:");
+            //Console.WriteLine(startState.ToString(false));
 
-			Console.WriteLine("Possible end states:");
-			while (true)
-			{
-				State state = startState;
+            //Console.WriteLine("Possible end states:");
+            while (true)
+            {
+                State state =  new State();//startState;
 
-				// Make a few moves
-				List<State> moves;
-				while ((moves = state.GetMoves()).Count > 0)
-				{
-					state = moves[r.Next(moves.Count)];
-				}
+                // Make moves until you've reached an end state
+                List<State> moves;
+                while ((moves = state.GetMoves()).Count > 0)
+                {
+                    //state = moves[r.Next(moves.Count)];
+                    state = WindowsScoreHeuristic(state, moves, r);
+                }
 
-				string stateText = state.ToString(true, 10);
-				Console.WriteLine(stateText);
-				StreamWriter writer = new StreamWriter(@"C:\Users\qub1\Desktop\Output.txt");
-				writer.Write(stateText);
-				writer.Close();
+                string stateText = state.ToString(true, 10);
+                Console.WriteLine(stateText);
+                StreamWriter writer = new StreamWriter(@"C:\Users\Wouter\Desktop\Output.txt");
+                writer.Write(stateText);
+                writer.Close();
 
-				Console.ReadLine();
-			}
-		}
-	}
+                Console.ReadLine();
+            }
+        }
+
+        static State WindowsScoreHeuristic(State state1, List<State> moves, RNG r)
+        {
+            int[] scores1 = GetScore(state1);
+            int highScore = -20;
+            List<State> bestStates = new List<State>();
+            foreach(State move in moves){
+                int[] scores = GetScore(move);
+                int tableauxDif = scores[1] - scores1[1];
+                int foundationDif = scores[2] - scores1[2];
+
+                // Waste/stock to tableaux/foundations
+                int score = Math.Abs(scores[0] - scores1[0]) * (5 * tableauxDif + 10 * foundationDif);
+                // Tableaux to foundations
+                if (tableauxDif == -1 && foundationDif == 1)
+                    score = 10;
+                // Foundations to tableaux
+                if (tableauxDif == 1 && foundationDif == -1)
+                    score = -15;
+                // Turn over a tableau card
+                if (scores[3] < scores1[3] || (tableauxDif == -1 && scores[3] == scores1[3]))
+                    score += 5;
+
+                if (score > highScore)
+                    bestStates = new List<State>() { move };
+                else if (score == highScore)
+                    bestStates.Add(move);
+            }
+            return bestStates[(int)r.Next(bestStates.Count)];
+        }
+
+        /// <summary>
+        /// Count the amount of cards in the stock/waste, foundations and tableaux
+        /// </summary>
+        /// <param name="s">The state you want to count for</param>
+        /// <returns>An integer array with: 0 = waste/stock-count, 1 = tableaux-count, 2 = foundations-count, 3 = amount of flipped cards</returns>
+        static int[] GetScore(State s)
+        {
+            int foundationScore = 0, tableauScore = 0, faceDownCount = 0;
+            foreach (CardStack tableau in s.Tableaus)
+            {
+                tableauScore += tableau.CardCount;
+                faceDownCount += tableau.FlippedCardCount;
+            }
+            foreach (CardStack foundation in s.Foundations)
+            {
+                foundationScore += foundation.CardCount;
+            }
+            return new int[4] { s.Stock.CardCount + s.Stock.Waste.CardCount, tableauScore, foundationScore, faceDownCount };
+        }
+    }
 }
