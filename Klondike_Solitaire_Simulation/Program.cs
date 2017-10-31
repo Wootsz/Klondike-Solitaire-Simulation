c7a491bfc23de14d29ab9518b7f09af1cbd9d181
Wootsz <wouterbrozius@hotmail.com>
2017-10-31 14:46:05 +0100

    Games die door alle heuristieken verloren zijn tellen niet mee

--- Klondike_Solitaire_Simulation/Program.cs
+++ Klondike_Solitaire_Simulation/Program.cs
@@ -8,195 +8,226 @@ using Klondike_Solitaire_Simulation.Heuristics;
 
 namespace Klondike_Solitaire_Simulation
 {
-	public class Program
-	{
-		private static void Main(string[] args)
-		{
-			Console.OutputEncoding = System.Text.Encoding.UTF8;
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-			Console.WriteLine("Welcome to the Klondike Solitaire Simulator!");
-			Console.Title = "Klondike Solitaire Simulator";
+            Console.WriteLine("Welcome to the Klondike Solitaire Simulator!");
+            Console.Title = "Klondike Solitaire Simulator";
 
-			RandomNumberGenerator r = new RandomNumberGenerator();
+            RandomNumberGenerator r = new RandomNumberGenerator();
 
-			// Initialize the game state
-			//State startState = new State();
+            // Initialize the game state
+            //State startState = new State();
 
-			//Console.WriteLine("Start state:");
-			//Console.WriteLine(startState.ToString(false));
+            //Console.WriteLine("Start state:");
+            //Console.WriteLine(startState.ToString(false));
 
-			//Console.WriteLine("Possible end states:");
-			Object stateLock = new Object();
-			Object writeLock = new Object();
-			Object iterationLock = new Object();
-			Object h1WincounterLock = new Object();
-			Object winPercentageLock = new Object();
+            //Console.WriteLine("Possible end states:");
+            Object stateLock = new Object();
+            Object writeLock = new Object();
+            Object iterationLock = new Object();
+            Object h1WincounterLock = new Object();
+            Object winPercentageLock = new Object();
 
             List<Heuristic> heuristics = new List<Heuristic>
             {
-				//new RandomHeuristic(),
-				//new TableauHeuristic(),
-				//new WindowsHeuristic(),
-				//new HighestRankHeuristic(),
-				new TableauHeuristic(),
-                //new ChanceHeuristic(),
-			};
+                new ChanceHeuristic(),
+                //new HighestRankHeuristic(),
+                new PlaceHoldersHeuristic(),
+                //new PlaceHolderHeuristic2(),
+                new RandomHeuristic(),
+                new TableauHeuristic(),
+                new WindowsHeuristic()
+            };
 
-			List<float> winPercentage = new List<float>(new float[heuristics.Count]);
+            int gamesNotLost = 0;
+            int h1Wincounter = 0;
+            int iteration = 1;
+            int iterations = 100;
+            List<float> winPercentage = new List<float>(new float[heuristics.Count]);
 
-			Parallel.For(0, heuristics.Count, heuristicIndex =>
-			//for (int heuristicIndex = 0; heuristicIndex < heuristics.Count; ++heuristicIndex)
-			{
-				Heuristic heuristic = heuristics[heuristicIndex];
+            Parallel.For(0, iterations, index =>
+            //for(int index = 0; index < iterations; ++index)
+            {
+                State startState;
+                lock (stateLock)
+                {
+                    startState = new State();
+                }
 
-				int h1Wincounter = 0;
-				int iteration = 1;
-				int iterations = 500;
+                List<bool> wonGames = new List<bool>();
+                string output = "";
 
-				Parallel.For(0, iterations, index =>
-				//for(int index = 0; index < iterations; ++index)
-				{
-					State state;
-					lock (stateLock)
-					{
-						state = new State(); //startState;
-					}
+                //Parallel.For(0, heuristics.Count, heuristicIndex =>
+                for (int heuristicIndex = 0; heuristicIndex < heuristics.Count; ++heuristicIndex)
+                {
+                    Heuristic heuristic = heuristics[heuristicIndex];
 
-					// Make moves until you've reached an end state
-					List<State> moves;
-					while (!state.IsWinState && (moves = state.GetMoves()).Count > 0)
-					{
-						state = heuristic.GetMove(state, moves);
-					}
+                    State state = new State(startState);
 
-					string output = "[" + heuristic.GetType().Name + "] Iteration " + iteration + "/" + iterations + " (" + index + "): ";
+                    // Make moves until you've reached an end state
+                    List<State> moves;
+                    while (!state.IsWinState && (moves = state.GetMoves()).Count > 0)
+                    {
+                        state = heuristic.GetMove(state, moves);
+                    }
 
-					lock (iterationLock)
-					{
-						++iteration;
-					}
+                    output += "[" + heuristic.GetType().Name + "] Iteration " + iteration + "/" + iterations + " (" + index + "): ";
 
-					// Check the type of state once done
-					if (state.IsWinState)
-					{
-						// If we won, add that
-						lock (h1WincounterLock)
-						{
-							++h1Wincounter;
-						}
+                    
 
-						output += "Win";
-					}
-					else
-					{
-						// Otherwise, show we lost
-						output += "Loss";
-					}
+                    wonGames.Add(state.IsWinState);
 
-					output += " after " + state.MovesMade + " moves";
+                    // Check the type of state once done
+                    if (state.IsWinState)
+                    {
+                        // If we won, add that
+                        lock (h1WincounterLock)
+                        {
+                            ++h1Wincounter;
+                        }
 
-					lock (writeLock)
-					{
-						Console.WriteLine(output);
-					}
+                        output += "Win";
+                    }
+                    else
+                    {
+                        // Otherwise, show we lost
+                        output += "Loss";
+                    }
 
-					//StreamWriter writer = new StreamWriter();
-					//string stateText = state.ToString(true, false);
-					//writer.WriteLine(stateText);
-					//writer.Flush();
-					//writer.Close();
+                    output += " after " + state.MovesMade + " moves\n";
 
-					//System.IO.File.WriteAllLines(@"C:\Users\Wouter\Desktop\Output\" + index + ".txt", state.ToString(true, false).Split('\n'));
-				});
+                    //lock (writeLock)
+                    //{
+                    // Console.WriteLine(output);
+                    //}
 
-				lock (winPercentageLock)
-				{
-					winPercentage[heuristicIndex] = (float)h1Wincounter / iterations * 100.0f;
-				}
-			});
+                    //StreamWriter writer = new StreamWriter();
+                    //string stateText = state.ToString(true, false);
+                    //writer.WriteLine(stateText);
+                    //writer.Flush();
+                    //writer.Close();
 
-			for (int i = 0; i < heuristics.Count; ++i)
-			{
-				Console.WriteLine("[" + heuristics[i].GetType().Name + "] Win percentage: " + winPercentage[i] + " % ");
-			}
+                    //System.IO.File.WriteAllLines(@"C:\Users\Wouter\Desktop\Output\" + index + ".txt", state.ToString(true, false).Split('\n'));
+                    //});
 
-			//Parallel.ForEach(Partitioner.Create(0, iterations), range =>
-			//{
-			//	for (int index = range.Item1; index < range.Item2; ++index)
-			//	{
-			//		State state = new State(); //startState;
+                    //lock (winPercentageLock)
+                    //{
+                    //    winPercentage[heuristicIndex] = (float)h1Wincounter / iterations * 100.0f;
+                    //}
+                }
 
-			//		// Make moves until you've reached an end state
-			//		List<State> moves;
-			//		while (!state.IsWinState && (moves = state.GetMoves()).Count > 0)
-			//		{
-			//			state = new WindowsHeuristic().GetMove(state, moves);
-			//		}
+                lock (iterationLock)
+                {
+                    ++iteration;
+                }
 
-			//		lock (writeLock)
-			//		{
-			//			Console.Write("Iteration " + iteration + "/" + iterations + ": ");
-			//			++iteration;
+                bool allLost = true;
+                for (int i = 0; i < wonGames.Count; i++)
+                {
+                    if (wonGames[i])
+                    {
+                        lock (winPercentageLock)
+                        {
+                            winPercentage[i]++;
+                        }
+                        allLost = false;
+                    }
+                }
 
-			//			// Check the type of state once done
-			//			if (state.IsWinState)
-			//			{
-			//				// If we won, add that
-			//				++h1Wincounter;
+                if (allLost)
+                    output = "All games lost\n";
+                else
+                    gamesNotLost++; 
 
-			//				Console.Write("Win");
-			//			}
-			//			else
-			//			{
-			//				// Otherwise, show we lost
-			//				Console.Write("Loss");
-			//			}
+                Console.Write(output);
+            });
 
-			//			Console.WriteLine(" after " + state.MovesMade + " moves");
-			//		}
-			//	}
-			//	/*
-			//	string stateText = state.ToString(true, 10);
-			//	Console.WriteLine(stateText);
-			//	StreamWriter writer = new StreamWriter(@"C:\Users\qub1\Desktop\Output.txt");
-			//	writer.Write(stateText);
-			//	writer.Close();
-			//             */
-			//});
+            for (int i = 0; i < heuristics.Count; ++i)
+            {
+                Console.WriteLine("[" + heuristics[i].GetType().Name + "] Win percentage: " + (float)(winPercentage[i]/gamesNotLost)*100 + " % ");
+            }
 
-			//Parallel.ForEach(Partitioner.Create(0, iterations), range =>
-			//{
-			//	for (int index = range.Item1; index < range.Item2; ++index)
-			//	{
-			//		State state = new State(); //startState;
-			//		Console.WriteLine(index);
+            //Parallel.ForEach(Partitioner.Create(0, iterations), range =>
+            //{
+            //	for (int index = range.Item1; index < range.Item2; ++index)
+            //	{
+            //		State state = new State(); //startState;
 
-			//		// Make moves until you've reached an end state
-			//		List<State> moves;
-			//		while ((moves = state.GetMoves()).Count > 0)
-			//		{
-			//			if (!state.IsWinState)
-			//				state = new WindowsHeuristic().GetMove(state, moves);
-			//			else if (state.IsWinState)
-			//			{
-			//				Interlocked.Increment(ref h1Wincounter);
-			//				Console.WriteLine("Win");
-			//				break;
-			//			}
+            //		// Make moves until you've reached an end state
+            //		List<State> moves;
+            //		while (!state.IsWinState && (moves = state.GetMoves()).Count > 0)
+            //		{
+            //			state = new WindowsHeuristic().GetMove(state, moves);
+            //		}
 
-			//			//state = moves[r.Next(moves.Count)];
-			//		}
-			//	}
-			//	/*
-			//	string stateText = state.ToString(true, 10);
-			//	Console.WriteLine(stateText);
-			//	StreamWriter writer = new StreamWriter(@"C:\Users\qub1\Desktop\Output.txt");
-			//	writer.Write(stateText);
-			//	writer.Close();
-			//             */
-			//});
+            //		lock (writeLock)
+            //		{
+            //			Console.Write("Iteration " + iteration + "/" + iterations + ": ");
+            //			++iteration;
 
-			Console.ReadLine();
-		}
-	}
+            //			// Check the type of state once done
+            //			if (state.IsWinState)
+            //			{
+            //				// If we won, add that
+            //				++h1Wincounter;
+
+            //				Console.Write("Win");
+            //			}
+            //			else
+            //			{
+            //				// Otherwise, show we lost
+            //				Console.Write("Loss");
+            //			}
+
+            //			Console.WriteLine(" after " + state.MovesMade + " moves");
+            //		}
+            //	}
+            //	/*
+            //	string stateText = state.ToString(true, 10);
+            //	Console.WriteLine(stateText);
+            //	StreamWriter writer = new StreamWriter(@"C:\Users\qub1\Desktop\Output.txt");
+            //	writer.Write(stateText);
+            //	writer.Close();
+            //             */
+            //});
+
+            //Parallel.ForEach(Partitioner.Create(0, iterations), range =>
+            //{
+            //	for (int index = range.Item1; index < range.Item2; ++index)
+            //	{
+            //		State state = new State(); //startState;
+            //		Console.WriteLine(index);
+
+            //		// Make moves until you've reached an end state
+            //		List<State> moves;
+            //		while ((moves = state.GetMoves()).Count > 0)
+            //		{
+            //			if (!state.IsWinState)
+            //				state = new WindowsHeuristic().GetMove(state, moves);
+            //			else if (state.IsWinState)
+            //			{
+            //				Interlocked.Increment(ref h1Wincounter);
+            //				Console.WriteLine("Win");
+            //				break;
+            //			}
+
+            //			//state = moves[r.Next(moves.Count)];
+            //		}
+            //	}
+            //	/*
+            //	string stateText = state.ToString(true, 10);
+            //	Console.WriteLine(stateText);
+            //	StreamWriter writer = new StreamWriter(@"C:\Users\qub1\Desktop\Output.txt");
+            //	writer.Write(stateText);
+            //	writer.Close();
+            //             */
+            //});
+
+            Console.ReadLine();
+        }
+    }
 }
